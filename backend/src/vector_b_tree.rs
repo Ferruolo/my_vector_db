0f56b4c6b00b097c42a7511f10bc41c313907d02
Andrew Ferrouolo <andrew.ferruolo@gmail.com>
2024-09-22 18:38:46 -0400

    Moved to new type of leaf node recursion to make room for mitosis:

--- backend/src/vector_b_tree.rs
+++ backend/src/vector_b_tree.rs
@@ -1,17 +1,34 @@
-use crate::vector_b_tree::TreeNode::{BranchNode, LeafNode, Null};
+use std::mem::swap;
+use crate::vector_b_tree::TreeNode::{BranchNode, LeafNode, Null, OverflowNode};
 
 const ELEMENTS_PER_PAGE: usize = 4;
 
 type DataType = String;
 type IndexType = usize;
-
 // Beautiful, functional code. Amazing, except it isn't totally functional (yet)
-
 
 // Can we combine these two similar items?
 struct BranchItem {
     indexes: Vec<IndexType>,
     data: Vec<TreeNode>,
+}
+
+fn binary_search<F>(vec: &[IndexType], comparator: F, index: &IndexType) -> usize
+where
+    F: Fn(&IndexType, &IndexType) -> bool,
+{
+    let mut low: usize = 0;
+    let mut high: usize = vec.len();
+
+    while low < high {
+        let mid = low + (high - low) / 2;
+        if comparator(&vec[mid], index) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    low
 }
 
 impl BranchItem {
@@ -40,29 +57,26 @@ impl LeafItem {
 enum TreeNode {
     LeafNode(LeafItem),
     BranchNode(BranchItem),
+    OverflowNode(Box<TreeNode>, IndexType, Box<TreeNode>),
     Null,
 }
 
 fn leaf_item_mitosis(mut node: LeafItem, index: IndexType, data: DataType) -> TreeNode {
     node = leaf_array_insertion(node, index, data);
-    let midpt = node.indexes[ELEMENTS_PER_PAGE.div_ceil(2)];
+    let midpt = ELEMENTS_PER_PAGE.div_ceil(2);
     let mut left = LeafItem::new();
     let mut right = LeafItem::new();
-    // might not have done this right, help!!!
-    for (arr_index, arr_data) in node.indexes.iter().zip(node.data) {
-        if arr_index < &midpt {
-            left.indexes.push(arr_index.clone());
-            left.data.push(arr_data);
-        } else {
-            right.indexes.push(arr_index.clone());
-            right.data.push(arr_data);
-        }
+    
+    assert_eq!(node.indexes.len(), node.data.len());
+    // We need to take ownership here
+    let cur_idx = node.indexes.len() - 1;
+    while let (Some(idx), Some(d)) = (node.indexes.pop(), node.data.pop()) {
+        let new_leaf = if cur_idx < midpt { &mut left } else { &mut right };
+        new_leaf.data.push(d);
+        new_leaf.indexes.push(idx);
     }
-    let mut new_branch = BranchItem::new();
-    new_branch.indexes.push(midpt);
-    new_branch.data.push(LeafNode(left));
-    new_branch.data.push(LeafNode(right));
-    BranchNode(new_branch)
+    // How do we split
+    OverflowNode(Box::new(LeafNode(left)), 0 /* TODO FIOX THIS*/, Box::new(LeafNode(right)))
 }
 
 //I never know if going with the imperative route or the functional route is better for the compiler
@@ -75,16 +89,29 @@ fn leaf_array_insertion(mut node: LeafItem, index: IndexType, data: DataType) ->
 //Make sure we take ownership here, no borrowing.   <- this may not scale
 fn insert_into_leaf_node(node: LeafItem, index: IndexType, data: DataType) -> TreeNode {
     if (node.indexes.contains(&index)) {
-        return LeafNode(node)
+        LeafNode(node)
     } else if (node.indexes.len() >= ELEMENTS_PER_PAGE) {
-        return leaf_item_mitosis(node, index, data)
+        leaf_item_mitosis(node, index, data)
     } else {
-        return LeafNode(leaf_array_insertion(node, index, data));
+        LeafNode(leaf_array_insertion(node, index, data))
     }
 }
 
-fn insert_into_branch_node(node: BranchItem, index: IndexType, data: DataType) -> TreeNode {
-    return Null 
+
+
+
+fn insert_into_branch_node(mut node: BranchItem, index: IndexType, data: DataType) -> (TreeNode, TreeNode) {
+    let idx = binary_search(&node.indexes, |left, right| {left < right}, &index);
+    let mut selected = Null;
+    swap(&mut selected, &mut node.data[idx]);
+    let result = match insert_item(selected, index, data) {
+        LeafNode(_) => {}
+        BranchNode(_) => {}
+        Null => {}
+    };
+    
+    
+    
 }
 
 
@@ -93,8 +120,8 @@ fn insert_item(node: TreeNode, index: IndexType, data: DataType) -> TreeNode {
         LeafNode(node) => {
             insert_into_leaf_node(node, index, data)
         }
-        TreeNode::BranchNode(node) => {
-            
+        BranchNode(node) => {
+            insert_into_branch_node(node, index, data)
         }
         Null => {
             let new_item = LeafItem::new();
