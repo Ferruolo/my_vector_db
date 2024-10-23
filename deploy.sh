dc7056d26ffc3561ba31e4b0a01faf73d7c9ee06
Andrew Ferruolo <andrew.ferruolo@gmail.com>
2024-10-23 13:44:58 -0400

    We have (untested) local deploy to kind

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 # Build images
-docker build -t lead-finder:latest -f Dockerfile.lead_finder ./scraper
-docker build -t scraper:latest -f Dockerfile.scraper ./scraper
-
-# Load into Kind cluster
-kind load docker-image lead-finder:latest --name lead-scraper-cluster
-kind load docker-image scraper:latest --name lead-scraper-cluster
-kind load docker-image redis:latest --name lead-scraper-cluster
-# Build images
+cd scraper
 docker build -t lead-finder:latest -f Dockerfile.lead_finder .
 docker build -t scraper:latest -f Dockerfile.scraper .
 
 # Load into Kind cluster
-kind load docker-image lead-finder:latest --name lead-scraper-cluster
-kind load docker-image scraper:latest --name lead-scraper-cluster
+kind load docker-image lead-finder:latest --name kind
+kind load docker-image scraper:latest --name kind
+kind load docker-image redis:latest --name kind
+# Build images
+docker build -t lead-finder:latest -f Dockerfile.lead_finder .
+docker build -t scraper:latest -f Dockerfile.scraper .
+
+cd ..
+# Load into Kind cluster
+kind load docker-image lead-finder:latest --name kind
+kind load docker-image scraper:latest --name kind
 kubectl apply -f manifest.yaml
