947efadd480728986e5243f0c1c3668374d3bb56
Andrew Ferruolo <andrew.ferruolo@gmail.com>
2024-11-01 18:35:45 -0400

    pRoMpT eNgInEeR

--- scraper/shared/llamafile_interface.py
+++ scraper/shared/llamafile_interface.py
@@ -1,18 +1,27 @@
 import requests
 from typing import Dict, List, Optional, Union
-import json
 
 PROMPT_extract_important_image_urls = """
+You are a web scraper tasked with analyzing the html output of a webpage. Given the 
+html of a webpage, you are tasked with identifying all important image links. You 
+will then return these image links if and only if they were in the original html, in the following JSON. 
+Please note that some important image links may link to a CDN or something similar.
+
+Make sure that you return your response in the following format, without any excess text
+{
+    'links': [https://myurl.com/path/to/image, https://myurl.com/path/to/image2] 
+}
 """
 
-PROMPT_extract_image_data = """
+PROMPT_extract_menu_data = """
+You are a menu scraper tasked with analyzing images and PDFs of menus. 
+You will take in the menu as an input, and you will return all menu items in the following JSON format:
 
+{'items': [
+        {'name': 'item name', 'price': '1234', 'type': 'APP/ENTREE/DRINK'},
+    ]
+}
 """
-
-PROMPT_test = """
-"""
-
-
 
 
 class LlamaFileWrapper:
@@ -67,9 +76,3 @@ class LlamaFileWrapper:
 
         return embeddings[0] if len(embeddings) == 1 else embeddings
 
-    def health_check(self) -> bool:
-        try:
-            response = requests.get(f"{self.base_url}/models")
-            return response.status_code == 200
-        except:
-            return False
