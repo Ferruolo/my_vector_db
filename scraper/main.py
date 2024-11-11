58970d5b917d73c8eb1f0cb15b1a8dfceb89f32e
Andrew Ferruolo <andrew.ferruoo@gmail.com>
2024-11-11 14:51:51 -0500

    Reformat

--- scraper/main.py
+++ scraper/main.py
@@ -2,17 +2,19 @@ import json
 import uuid
 from typing import List
 from uuid import uuid4
+
 import pandas as pd
 from cassandra.cluster import Cluster, Session
+
 from scrape_website.website_scraper import get_all_links, is_pdf_link
 from scrape_website.yelp_interface import YelpInterface
+from shared.extra_apis import get_coordinates
 from shared.helpers import drop_repeated_newline_regex
 from shared.llm_wrapper import LlamafileWrapper, ClaudeWrapper
-from shared.models import MenuItem, Menu, Location
+from shared.models import Menu, Location
 from shared.prompts import format_extract_all_important_links
 from shared.put_data_to_cassandra import insert_business, insert_menu_item, insert_business_location, insert_text_data
 from shared.redis_interface import create_redis_client, create_channel_interface
-from shared.extra_apis import get_coordinates
 
 table_name = "restaurant_inspections_indexed"
 index_name = "SCRAPER_IDX"
@@ -31,11 +33,8 @@ def put_menu_items(session, menu: Menu, biz_id):
 def put_biz_locations(session, locations: List[Location], biz_id):
     for location in locations:
         lat, long = get_coordinates(location)
-        insert_business_location(session, uuid4(), biz_id, lat, long,
-                                 location.building_number,
-                                 location.street,
-                                 location.room_number,
-                                 location.city, location.state)
+        insert_business_location(session, uuid4(), biz_id, lat, long, location.building_number, location.street,
+                                 location.room_number, location.city, location.state)
 
 
 def put_chunks(session: Session, biz_id, embeddings: List[(str, List[float])]):
@@ -115,28 +114,17 @@ def main() -> None:
             biz_id = uuid.uuid4()
             try:
                 # Use try catch to add atomicity (makes it easier to keep everything straight)
-                insert_business(
-                    session,
-                    biz_id,
-                    selected['name'],
-                    selected['id'],
-                    "pickup" in selected['transactions'],
-                    "delivery" in selected['transactions'],
-                    selected['rating'],
-                    calc_price_magnitude(selected['price']),
-                    selected['phone'],
-                    url
-                )
+                insert_business(session, biz_id, selected['name'], selected['id'], "pickup" in selected['transactions'],
+                                "delivery" in selected['transactions'],
+                                selected['rating'], calc_price_magnitude(selected['price']), selected['phone'], url)
                 put_menu_items(session, structured_data.menu, biz_id)
                 put_biz_locations(session, structured_data.locations, biz_id)
                 put_chunks(session, biz_id, text_data)
 
-
             except KeyboardInterrupt:
                 exit(1)
             except Exception as e:
-                print(f"{row['name']} failed with {e}")
-                # TODO: Create list of all failures in redis
+                print(f"{row['name']} failed with {e}")  # TODO: Create list of all failures in redis
 
 
 if __name__ == "__main__":
